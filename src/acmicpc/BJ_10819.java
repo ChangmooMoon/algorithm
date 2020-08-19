aa2c77e59de7d393e10621797b4ca49f6ae85f8a
changmoomoon <public.stevemoon@gmail.com>
2020-08-19 23:46:41 +0900

    bfs

--- src/acmicpc/BJ_10819.java
+++ src/acmicpc/BJ_10819.java
@@ -1,57 +1,33 @@
 package acmicpc;
 
 import java.io.BufferedReader;
-//import java.io.FileReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class BJ_10819 {
-  public static void main(String[] args) throws IOException {
-//    BufferedReader br = new BufferedReader(new FileReader("src/INPUT.txt"));
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+  static int[] a;
+  static int prev;
+  static int sum = 0;
+  static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new FileReader("src/INPUT.txt"));
+//    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int n = Integer.parseInt(br.readLine());
+    a = new int[n];
+
     StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-    List<Integer> list = new ArrayList<>(n);
     for (int i = 0; i < n; i++) {
-      int a = Integer.parseInt(st.nextToken());
-      list.add(a);
+      a[i] = Integer.parseInt(st.nextToken());
     }
-    Collections.sort(list);
-    int result = Integer.MIN_VALUE;
-    do {
-      int tmp = calculate(list);
-      result = Math.max(result, tmp);
-    } while (next_permutation(list));
 
-    System.out.println(result);
+    backtracking(n,0);
+    System.out.println(sum);
   }
 
-  static int calculate(List<Integer> list) {
-    int sum = 0;
-    for (int i = 1; i < list.size(); i++) {
-      sum += Math.abs(list.get(i-1) - list.get(i));
+  static void backtracking(int n, int prev) {
+    if (n == 0) {
+
     }
-    return sum;
-  }
-
-  static boolean next_permutation(List<Integer> list) {
-    int i = list.size() - 1;
-    int j = list.size() - 1;
-
-    while(i > 0 && list.get(i-1) >= list.get(i)) i--;
-    if(i<=0) return false;
-
-    while(list.get(i-1) > list.get(j)) j--;
-    Collections.swap(list, i - 1, j);
-
-    j = list.size() -1;
-    while (i < j) {
-      Collections.swap(list, i, j);
-      i++;
-      j--;
-    }
-    return true;
   }
 }
